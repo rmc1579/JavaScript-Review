6b296318895c270bd3255ef0d1ffb52e39951225
rmc1579 <rmc1579@gmail.com>
2015-10-03 13:42:39 -0600

    Adding callbackReview.js file with answers

--- callbackReview.js
+++ callbackReview.js
@@ -1,6 +1,8 @@
 /* Declare and Define the functions here that will make the function calls below work properly */
 
-
+var first = function(n,cb){
+  return cb(n[0]);
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
@@ -12,7 +14,9 @@ first(names, function(firstName){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+var last = function(n,cb){
+  return cb(n[n.length-1]);
+};
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -27,6 +31,16 @@ last(names, function(lastName){
 
 //have the contains function return a boolean value for if the name is in the array or not.
 
+var contains = function(n,narray,cb){
+    
+  for (var i=0; i<narray.length; i++){
+    if (narray[i] === n){     
+       cb(true);
+       return;
+    }
+  }
+  cb(false);
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains('Colt', names, function(yes){
@@ -42,7 +56,12 @@ contains('Colt', names, function(yes){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+var map = function(numarray, cb){
+  for (var i=0; i<numarray.length; i++){
+    console.log(cb(numarray[i]));
+  }
+  
+};
 
 
 var numbers = [1,2,3,4,5];
@@ -52,12 +71,20 @@ map(numbers, function(num){
 });
 
 
-
-
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
-
+var uniq = function(namearray, cb){
+  var newarray = [];
+  var obj = {};
+  for (var i = 0; i<namearray.length; i++){
+    obj[namearray[i]] = 0;
+  }
+  for (var key in obj){
+    newarray.push(key);
+  }
+  return newarray;
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
@@ -69,12 +96,17 @@ uniq(names, function(uniqArr){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+var each = function(namearray, cb){
+  for (var i = 0; i<namearray.length; i++){
+    cb(namearray[i], i);
+  }
+  return;
+};
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
@@ -82,7 +114,13 @@ each(names, function(item, indice){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+var getUserById = function(str,array,cb){
+  for (var i=0; i<array.length; i++){
+    if (str === array[i].id){
+      return cb(array[i]);
+    }
+  }
+};
 
 
 var users = [
@@ -118,6 +156,14 @@ getUserById('16t', users, function(user){
 
 
 //Looks through each value in the list, returning the first one that passes a truth test 
+var find = function(narray, cb){
+  for (var i=0; i<narray.length; i++){
+    if (narray[i] % 2 === 0){
+      return console.log(narray[i]);
+    }
+  }
+};
+
 var numbers  = [1, 2, 3, 4, 5, 6];
 find(numbers, function(num){ 
   return num % 2 == 0; //should return 2
